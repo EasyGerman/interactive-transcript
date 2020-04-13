1d5108e37c3671e7b75bbd36aebdf6eb26c08e0a
Levente Bagi <bagilevi@gmail.com>
2020-04-13 20:29:40 +0200

    translation

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'nokogiri'
 gem 'redis'
+gem 'faraday'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :episodes, only: [:index, :show]
+  post :translate, to: 'translate#translate'
   root to: 'main#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
