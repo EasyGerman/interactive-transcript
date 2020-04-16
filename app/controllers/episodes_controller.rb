2661d1ca7f7c568901dd100b8ee64df842c6a057
Levente Bagi <bagilevi@gmail.com>
2020-04-16 20:12:25 +0200

    find episode in feed by unique key

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,8 +4,32 @@ class EpisodesController < ApplicationController
   end
 
   def show
+    # Find episode
+
+    episode_key = params[:id]
+
+    # - if any of these is present in the description => pick that episode
+    identifier_strings = [
+      "_transkript_#{episode_key}.html",
+      "https://easygermanpodcastplayer.herokuapp.com/episodes/#{episode_key}.html",
+      episode_url(episode_key),
+    ].uniq
+
     @xml = Nokogiri::XML(rss)
-    @episode = @xml.css('item')[params[:id].to_i - 1]
+
+    # - search through all episodes
+    @episode = @xml.css('item').find do |episode_node|
+      description_html = episode_node.css('description').text
+
+      identifier_strings.any? do |identifier_string|
+        description_html.include?(identifier_string)
+      end
+    end
+
+    if @episode.blank?
+      raise ActionController::RoutingError.new('Episode not found')
+    end
+
     @title = @episode.css('title').first.text
     @html = Nokogiri::HTML(@episode.css('description').text)
 
