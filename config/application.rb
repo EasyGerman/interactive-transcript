b02b6b6652ced621b52cea30c1f61fd9ef99130c
Levente Bagi <bagilevi@gmail.com>
2020-04-16 20:12:36 +0200

    load dotenv before Application

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "dotenv"
+require "dotenv-rails"
+Dotenv::Railtie.load
+
 module Player
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
