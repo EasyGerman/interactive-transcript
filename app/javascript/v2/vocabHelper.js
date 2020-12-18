e6c840fb6ca42e1b2892279b48623bc296cc910b
Levente Bagi <bagilevi@gmail.com>
2020-12-18 07:49:38 +0100

    fix: vocabHelper when no chapter

--- app/javascript/v2/vocabHelper.js
+++ app/javascript/v2/vocabHelper.js
@@ -11,6 +11,7 @@ function init(params) {
 
 function setImgSrc($img, chapterId) {
   $img.off('error');
+  // TODO: multi-podcast: dynamic URL format
   const primaryURL = `https://easygermanpodcastplayer-public.s3.eu-central-1.amazonaws.com/vocab/${accessKey}/${chapterId}.jpg`;
   const fallbackURL = `/episodes/${accessKey}/chapters/${chapterId}/picture.jpg`
   $img.attr('src', primaryURL);
@@ -24,22 +25,32 @@ function setImgSrc($img, chapterId) {
 
 function ontimeupdate(e) {
   const eventTs = media.currentTime;
+
+  // Find the current chapter
   const chapter = chapters.find(({ id, start_time, end_time, has_picture }, index) => {
-    return start_time < eventTs && end_time > eventTs;
+    return start_time <= eventTs && end_time > eventTs;
   });
 
+  // If we just entered a new chapter
   if (chapter !== activeChapter) {
+    console.log('Chapter changed:', chapter);
     activeChapter = chapter;
+
     if (chapter && chapter.has_picture) {
+      // Show a vocab helper slide
       $img.attr('src', ''); // clear the image to avoid transition artifacts when we change 'is-cover'
       setImgSrc($img, chapter.id);
     } else {
+      // Show podcast cover
       $img.attr('src', coverUrl);
       $img.addClass('is-cover');
     }
 
+    // Preload next vocab helper
     const nextChapter = chapters.find(({ id, start_time, end_time, has_picture }, index) => {
-      return start_time > activeChapter.start_time && has_picture;
+      // If there's no current chapter => select the first chapter that has a picture;
+      // otherwise select the one that comes after the current chapter.
+      return (activeChapter ? start_time > activeChapter.start_time : true) && has_picture;
     });
     if (nextChapter) {
       setImgSrc($(new Image()), nextChapter.id);
