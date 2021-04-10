a75f26ae6e0d19363055560af07fae4044ad398e
Levente Bagi <bagilevi@gmail.com>
2021-04-10 15:54:17 +0200

    feat: show interface in English

--- app/controllers/concerns/podcast_site_controller_concern.rb
+++ app/controllers/concerns/podcast_site_controller_concern.rb
@@ -7,7 +7,6 @@ module PodcastSiteControllerConcern
 
   included do
     before_action :current_podcast
-    around_action :switch_locale
 
     rescue_from PodcastNotFound do |e|
       render plain: "Error 404 - Not found (host not found: #{e.host})", status: 404
@@ -23,13 +22,5 @@ module PodcastSiteControllerConcern
         )
     end
 
-    def switch_locale(&action)
-      locale = current_podcast.locale || I18n.default_locale
-      if locale == 'de'
-        I18n.with_locale(locale, &action)
-      else
-        action.call
-      end
-    end
   end
 end
