b2751489229d1c516b0a853d3d1a8bec544e825a
Levente Bagi <bagilevi@gmail.com>
2021-04-17 13:02:00 +0200

    fix: add semantic.js to main bundle

--- app/javascript/packs/v2.js
+++ app/javascript/packs/v2.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("channels")
 require("jquery")
+require("../semantic")
 require("../v2/player")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
